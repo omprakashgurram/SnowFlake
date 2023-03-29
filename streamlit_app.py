5d412db5806a8e5f2e0e1be97241eada992c5d95
omprakashgurram <125947229+omprakashgurram@users.noreply.github.com>
2023-03-29 18:34:20 +0530

    Update streamlit_app.py
    added the header and change in output

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title("My parents new healthy dinner")
-streamlit.text("Breakfast menu")
-streamlit.text("Omega 3 and blueberry oatmeal")
-streamlit.text("Kale , spinach and rocket smoothie")
-streamlit.text("Hard-Boilde Free-Range Egg")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
