26ebf7c5abcd6ee620bfb4a53259a62bb9f19e4f
omprakashgurram <125947229+omprakashgurram@users.noreply.github.com>
2023-04-03 11:35:14 +0530

    Update streamlit_app.py
    added close command and changed the headers

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ except URLError as e:
 
 #dont run anything in the past
 
-streamlit.header("The Fruit Load List Contains")
+streamlit.header("View Our Fruit List - Add Your Favourites!")
 #snowflake-related functions
 
 def get_fruit_load_list():
@@ -57,9 +57,10 @@ def get_fruit_load_list():
     return my_cur.fetchall()
 
 #add a button to load the fruit
-if streamlit.button('Get fruit load list'):
+if streamlit.button('Get fruit list'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
+  my_cnx.close()
   streamlit.dataframe(my_data_rows)
 
 #streamlit.stop()
@@ -72,4 +73,5 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add fruit to the list'):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    back_from_function = insert_row_snowflake(add_my_fruit)
+   my_cnx.close()
    streamlit.text(back_from_function)
