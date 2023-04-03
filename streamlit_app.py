165ac89b09d160acd2759fa263aca765f0109757
omprakashgurram <125947229+omprakashgurram@users.noreply.github.com>
2023-04-03 11:29:25 +0530

    Update streamlit_app.py
    commented stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,7 +62,7 @@ if streamlit.button('Get fruit load list'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
-streamlit.stop()
+#streamlit.stop()
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
     my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.fruit_load_list values ('from streamlit');")
